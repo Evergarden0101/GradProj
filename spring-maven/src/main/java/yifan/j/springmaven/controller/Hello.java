0ceb7688155098b6a994b7a4b0a51998a12e36d2
Yifan Jiang <jimmyjiang1999@gmail.com>
2021-03-16 15:25:56 +0800

    gradproj

---
+++ spring-maven/src/main/java/yifan/j/springmaven/controller/Hello.java
@@ -0,0 +1,15 @@
+package yifan.j.springmaven.controller;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@ConfigurationProperties(prefix=)
+@RestController
+public class Hello {
+    @RequestMapping("/hello")
+
+    public String handle() {
+        return "Hello World!";
+    }
+}
